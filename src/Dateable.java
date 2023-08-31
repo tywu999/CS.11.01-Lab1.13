d812acaaac9f3a46abfaa7eb9192084a36d4a955
techarenz <57818506+techarenz@users.noreply.github.com>
2023-08-31 11:53:19 +0800

    Update README.md

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
+
 public class Dateable {
 
-    public static void main(String[] args) {
-        
+    public static void main (String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please Enter Your Age:");
+        int age = scanner.nextInt();
+        int divide = (age/2);
+        System.out.println("You should only date " + (7+divide) + " year olds or above");
     }
+
 }
--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
+
 public class Pages {
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please Enter Your Age:");
+        int age = scanner.nextInt();
+        System.out.println("You should read at least " + (100-age) + " pages of a book before you give up");
     }
 }
